4ccf3da85860f8734959e6c52ad1abb63a091e7f
pameirelles <rafaelpmeirelles@gmail.com>
2022-12-29 16:04:33 -0300

    Detailed Move

--- move.py
+++ move.py
@@ -10,3 +10,13 @@ class Move:
 
     def to_register(self):
         return f"{self.who} {self.end} {self.block}"
+
+
+class Detailed(Move):
+    def __init__(self, who, begin, end, block, rise, height, center):
+        super().__init__(who, begin, end, block)
+        self.rise = rise
+        self.height = height
+        self.center = center
+
+
