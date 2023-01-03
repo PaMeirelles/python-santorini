6820ef6a2b1d8526b1b5db092d9dc1e19664355f
pameirelles <rafaelpmeirelles@gmail.com>
2023-01-03 18:27:08 -0300

    Reindexing

--- temp.py
+++ temp.py
@@ -1,3 +1,8 @@
 import pandas as pd
+from os import remove, rename
 
 df = pd.read_csv("meta/matches")
+bonus = 0
+size = df.shape[0]
+for i in range(3192, 3303):
+    rename(f"games/{i}", f"games/{i - 110}")
