b54e71785b5e4109999ec69af3e8d8b79743a084
pameirelles <rafaelpmeirelles@gmail.com>
2023-01-04 14:31:13 -0300

    Removed unnamed index

--- temp.py
+++ temp.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from board import fix_hash, unhash_position
 
 df = pd.read_csv("meta/matches")
-df["starting_pos"] = df["starting_pos"].apply(fix_hash)
-df.to_csv("meta/matches")
+del df["Unnamed: 0.1"]
+del df["Unnamed: 0"]
+print(df.columns)
+df.to_csv("meta/matches", index=False)
